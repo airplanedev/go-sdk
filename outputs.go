15837744af3b07a7b56f5dfdeab715b783c91975
Colin King <me@colinking.co>
2021-03-31 17:34:25 -0500

    more docs

--- outputs.go
+++ outputs.go
@@ -8,16 +8,34 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Output writes `value` as an Airplane output to stdout. Outputs are
+// separated from your logs and used to provide context structured
+// context to end-users of your task.
+//
+// Docs: https://docs.airplane.dev/reference/outputs
 func Output(value interface{}) error {
 	return NamedOutput("", value)
 }
 
+// MustOutput writes `value` to stdout as an Airplane output. Outputs are
+// separated from your logs and used to provide context structured
+// context to end-users of your task.
+//
+// If an error is produced, MustOutput will panic.
+//
+// Docs: https://docs.airplane.dev/reference/outputs
 func MustOutput(value interface{}) {
 	if err := Output(value); err != nil {
 		panic(errors.Wrap(err, "writing output"))
 	}
 }
 
+// NamedOutput writes `value` to stdout as an Airplane output. Outputs are
+// separated from your logs and used to provide context structured
+// context to end-users of your task. Unlike Output, NamedOutput accepts
+// a `name` which will be used to group separate streams of outputs together.
+//
+// Docs: https://docs.airplane.dev/reference/outputs
 func NamedOutput(name string, value interface{}) error {
 	if err := validateOutputName(name); err != nil {
 		return err
@@ -38,6 +56,14 @@ func NamedOutput(name string, value interface{}) error {
 	return nil
 }
 
+// MustNamedOutput writes `value` to stdout as an Airplane output. Outputs are
+// separated from your logs and used to provide context structured
+// context to end-users of your task. Unlike Output, NamedOutput accepts
+// a `name` which will be used to group separate streams of outputs together.
+//
+// If an error is produced, MustNamedOutput will panic.
+//
+// Docs: https://docs.airplane.dev/reference/outputs
 func MustNamedOutput(name string, value interface{}) {
 	if err := NamedOutput(name, value); err != nil {
 		panic(errors.Wrapf(err, "writing output %s", name))
